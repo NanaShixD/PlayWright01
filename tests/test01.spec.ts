4cbcf8ba5ab25677019c6f006887d67528e5dae5
NanaShixD <SzPilanos1993@gmail.com>
2023-08-24 19:59:16 +0200

    Update test01.spec.ts

--- tests/test01.spec.ts
+++ tests/test01.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('/');
-  await page.locator('#login_id').click();
+  //await page.locator('#login_id').click();
   await page.locator('#login_id').fill('12345678');
   await page.locator('#login_id').blur();
   await page.getByRole('button', { name: 'dalej' }).click();
