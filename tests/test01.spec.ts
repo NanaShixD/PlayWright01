c17f41dfc638e2ddfc20378c322b3d60b03cb930
NanaShixD <SzPilanos1993@gmail.com>
2023-08-30 21:55:20 +0200

    + credki

--- tests/test01.spec.ts
+++ tests/test01.spec.ts
@@ -1,14 +1,19 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Login path',() => {
+//Credki
+const Login = 12345678
+const Password = qweasdzx
+
+
 test('Good login path', async ({ page }) => {
   await page.goto('/');
   await page.locator('#login_id').click();
-  await page.locator('#login_id').fill('12345678');
+  await page.locator('#login_id').fill(Login);
   await page.locator('#login_id').blur();
   await page.getByRole('button', { name: 'dalej' }).click();
   await page.locator('#login_password').click();
-  await page.locator('#login_password').fill('qweasdzx');
+  await page.locator('#login_password').fill(Password);
   await page.locator('#login_password').blur();
   await page.getByRole('button', { name: 'zaloguj się' }).click();
 });
