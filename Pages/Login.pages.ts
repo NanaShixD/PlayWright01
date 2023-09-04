eb8156d125b8cf955da3482a0f0fa121380bfd7a
NanaShixD <SzPilanos1993@gmail.com>
2023-09-04 20:06:40 +0200

    add pages const

--- Pages/Login.pages.ts
+++ Pages/Login.pages.ts
@@ -3,6 +3,9 @@ import { Page } from "@playwright/test";
 export class LoginPage{
 constructor(private page: Page) {}
 
-logininput = this.page.locator('#login_id')
+loginInput = this.page.locator('#login_id')
+passwordInput = this.page.locator('#login_password')
+nextButton = this.page.getByRole('button', { name: 'dalej' })
+loginButton = this.page.getByRole('button', { name: 'zaloguj się' })
 
 }
