8a9bd947de785a0162e16b2f547c86f804268578
NanaShixD <SzPilanos1993@gmail.com>
2023-09-04 20:27:08 +0200

    formatet

--- Pages/Login.pages.ts
+++ Pages/Login.pages.ts
@@ -1,11 +1,11 @@
 import { Page } from "@playwright/test";
 
-export class LoginPage{
-constructor(private page: Page) {}
+export class LoginPage {
+    constructor(private page: Page) { }
 
-loginInput = this.page.locator('#login_id')
-passwordInput = this.page.locator('#login_password')
-nextButton = this.page.getByRole('button', { name: 'dalej' })
-loginButton = this.page.getByRole('button', { name: 'zaloguj się' })
+    loginInput = this.page.locator('#login_id')
+    passwordInput = this.page.locator('#login_password')
+    nextButton = this.page.getByRole('button', { name: 'dalej' })
+    loginButton = this.page.getByRole('button', { name: 'zaloguj się' })
 
 }
--- tests/test01.spec.ts
+++ tests/test01.spec.ts
@@ -1,20 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Login path',() => {
-//Credki logowania
-const Login = '12345678'
-const Password = 'qweasdzx'
+test.describe('Login path', () => {
+  //Credki logowania
+  const Login = '12345678'
+  const Password = 'qweasdzx'
 
 
-test('Good login path', async ({ page }) => {
-  await page.goto('/');
-  await page.locator('#login_id').click();
-  await page.locator('#login_id').fill(Login);
-  await page.locator('#login_id').blur();
-  await page.getByRole('button', { name: 'dalej' }).click();
-  await page.locator('#login_password').click();
-  await page.locator('#login_password').fill(Password);
-  await page.locator('#login_password').blur();
-  await page.getByRole('button', { name: 'zaloguj się' }).click();
-});
+  test('Good login path', async ({ page }) => {
+    await page.goto('/');
+    await page.locator('#login_id').click();
+    await page.locator('#login_id').fill(Login);
+    await page.locator('#login_id').blur();
+    await page.getByRole('button', { name: 'dalej' }).click();
+    await page.locator('#login_password').click();
+    await page.locator('#login_password').fill(Password);
+    await page.locator('#login_password').blur();
+    await page.getByRole('button', { name: 'zaloguj się' }).click();
+  });
 });
